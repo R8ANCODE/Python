addc4bc9448279ddc9e708dda4d0d182f6db2f7e
R8ANCODE <aksarasanirayyan@gmail.com>
2025-04-09 16:08:13 +0700

    Data types

---
+++ Data types.py
@@ -0,0 +1,52 @@
+x = "Welcome to python 3" #Text type/str
+
+print(x) #Without this, there will be no output
+
+print(type(x))
+
+#or
+
+x = 5 #int
+
+print(x) 
+
+print(type(x)) 
+
+
+x = 5.5 #float
+
+print(x) 
+
+print(type(x)) 
+
+x = "30a" #complex
+
+print(x) 
+
+print(type(x)) 
+BOMB = ["KACHOWW", "BOM", "YEOWWW"] #list tuple
+
+print(BOMB)
+
+print(type(BOMB))
+
+#or
+
+print(type(x)) 
+BOMB = ("KACHOWW", "BOM", "YEOWWW") #list tuple
+
+print(BOMB)
+
+print(type(BOMB))
+
+x = { 'name' : 'John', 'age' : 36 } #dict/dictionaries
+
+print(x)
+
+print(type(x))
+
+x = (5 > 7)
+
+print(x)
+
+print(type(x))
