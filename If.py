445a7f5f307ad1ae8462aeed3fd5ae269631d7e4
R8ANCODE <aksarasanirayyan@gmail.com>
2025-04-16 16:17:12 +0700

    if statements yay
    yes2

---
+++ If.py
@@ -0,0 +1,19 @@
+num = 3
+if num > 0:
+    print(num, "is a positive number")
+print("This is always printed")  
+
+num = -3
+
+if num >= 0:
+    print("Positive or Zero")
+else:
+    print("Negative Number")
+
+num = 0
+if num > 0:
+    print("Positive number")
+elif num == 0:
+    print("Zero")
+else:
+    print("Negative number")              
